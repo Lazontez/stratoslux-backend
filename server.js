15e0ce1620b0f366e93455d2c097b8e76afca52c
Lazontez <tezgardner1212@gmail.com>
2025-03-20 05:10:53 -0500

    working through issue with email

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ pool.query(createTableQuery)
 
 const sendEmail = async (booking) => {
   const defaultClient = SibApiV3Sdk.ApiClient.instance;
-  const apiKey = defaultClient.authentications[process.env.SENDINBLUE_API_KEY];
+  const apiKey = defaultClient.authentications['api-key'];
   apiKey.apiKey = process.env.SENDINBLUE_API_KEY;
 
   const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
